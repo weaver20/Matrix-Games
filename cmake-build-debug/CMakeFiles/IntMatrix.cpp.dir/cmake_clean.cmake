ad0fe4ee6343334288604aabb9be80f28fc226f3
aviram <aviramsh@campus.technion.ac.il>
2020-06-07 11:56:33 +0300

    BACKUP Before formatting

---
+++ cmake-build-debug/CMakeFiles/IntMatrix.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IntMatrix.cpp.dir/Auxiliaries.cpp.obj"
+  "CMakeFiles/IntMatrix.cpp.dir/test_partA.cpp.obj"
+  "IntMatrix.cpp.exe"
+  "IntMatrix.cpp.exe.manifest"
+  "IntMatrix.cpp.pdb"
+  "libIntMatrix.cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IntMatrix.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/IntMatrix.cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for IntMatrix.cpp.
+# This may be replaced when dependencies are built.
