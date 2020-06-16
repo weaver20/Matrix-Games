f4495d32b4d6ac95ec185f2784472a8b717fb209
Aviram Shubeili <aviramshovali@gmail.com>
2020-06-16 16:53:22 +0300

    Update .h file

---
+++ PartB/Matrix.h
@@ -0,0 +1,21 @@
+#ifndef EX3_MATRIX_H
+#define EX3_MATRIX_H
+#include "../PartA/Auxiliaries.h"
+
+
+// use to check and simplify implementation
+typedef int T;
+
+namespace mtm {
+    template<class T>
+    class Matrix {
+    Dimensions dimensions;
+    T* data;
+
+    public:
+
+    };
+}
+
+
+#endif //EX3_MATRIX_H
