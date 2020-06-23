88803ab911b10bc878eccb35b3ac9c0aef3b1e92
Aviram Shubeili <aviramshovali@gmail.com>
2020-06-23 11:04:48 +0300

    Import updated partB files from server Delete Matrix.cpp

--- PartB/test_partB.cpp
+++ PartB/test_partB.cpp
@@ -31,7 +31,7 @@ int main(){
         const mtm::Matrix<int> mat_1 = mtm::Matrix<int>::Diagonal(2,1);
         mtm::Matrix<int> mat_2 = mtm::Matrix<int>::Diagonal(2,-1);
         std::cout<<mtm::any(mat_1)<<" "<<mtm::any(mat_1-mat_2)<<" "<<mtm::all(mat_1+mat_2+1)<<std::endl;
-        std::cout<<(-mat_2)(1,1)<<(-mat_2)(2,2)<<std::endl;
+        std::cout<<(-mat_2)(2,2)<<std::endl;
     } catch(mtm::Matrix<int>::AccessIllegalElement& e){
         std::cout<<e.what()<<std::endl;
     }
